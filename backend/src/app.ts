fd746a69f8bb3d87b8dddc2efe8d988049f772d4
Vyannnnnn <fian88518@gmail.com>
2026-09-23 18:37:41 +0700

    fix cors 2

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,10 +4,29 @@ import routes from "./routes";
 import { errorHandler } from "./middlewares/error";
 
 const app = express();
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || "*" || "https://tes-indokerja-app-frontend.vercel.app",
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: process.env.CORS_ORIGIN || "*" || "https://tes-indokerja-app-frontend.vercel.app",
+//   credentials: true,
+// }));
+
+const allowedOrigins = [
+  "https://indokerja-frontend-psi.vercel.app",
+  "https://tes-indokerja-app-frontend.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use("/api", routes);
 app.use(errorHandler);
